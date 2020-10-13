8c3b626ceb708bacc08d7e0f724634037f9fc2f0
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-13 09:51:34 +0530

    Add: Creating board for game

---
+++ TicTacToeGame/TicTacToeGame/TicTacToeGameClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class TicTacToeGameClass
+    {
+        static void Main(string[] args)
+        {
+            char[] board = CreationBoard();
+        }
+        public static char[] CreationBoard()
+        {
+            char[] board = new char[10];
+            for(int j=0; j<board.Length; j++)
+            {
+                board[j] = ' ';
+            }
+            return board;
+        }
+    }
+}
